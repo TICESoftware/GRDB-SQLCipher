0db30479a8ee706949edfbe36b4ab6d76292c335
Mohamad <mo.rajbi@gmail.com>
2024-10-28 20:29:09 +0330

    Inline GRDB.swift 3.0.6 (GRDB 7.0.0-beta.6, SQLCipher 4.6.1)

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "GRDB",
-            url: "https://github.com/inlinehq/GRDB.swift/releases/download/3.0.5/GRDB.xcframework.zip",
-            checksum: "265a37cf6b77c5301d0c2c38823e18842c8833b386d0a502003096029a723525"
+            url: "https://github.com/inlinehq/GRDB.swift/releases/download/3.0.6/GRDB.xcframework.zip",
+            checksum: "e8b85b64f120a0715eeac1e2b85776974316bb4c32602d9e789b64e00c90e8eb"
         ),
         .target(name: "_GRDBDummy")
     ]
